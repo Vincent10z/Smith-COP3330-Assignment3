bd9df9d59e9b434c28ebbc9ca39abab9a3856a86
Vincent10z <Vincent10z@yahoo.com>
2021-06-20 19:15:16 -0400

    Fixes/changes/tests

---
+++ src/test/java/EX42/ex42TestAUTO.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ex42TestAUTO {
+
+    @Test
+    void getInfo() {
+    }
+
+    @Test
+    void setInfo() {
+    }
+
+    @Test
+    void writeInfo() {
+    }
+
+    @Test
+    void main() {
+    }
+}
